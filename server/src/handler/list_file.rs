9f9dad006643a0cfe0096bfb0af1084d87f0bfe2
booiris <954520770@qq.com>
2023-05-13 00:04:34 +0800

    feat: add ls

--- server/src/handler/list_file.rs
+++ server/src/handler/list_file.rs
@@ -1,4 +1,4 @@
-use axum::Json;
+use axum::{extract::Query, Json};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Default)]
@@ -13,22 +13,23 @@ pub struct LsResponse {
     pub files: Vec<String>,
 }
 
-pub async fn ls(Json(req): Json<LsRequest>) -> Result<Json<LsResponse>, String> {
+pub async fn ls(Query(req): Query<LsRequest>) -> Result<Json<LsResponse>, String> {
     let base = std::env::current_dir()
-        .map_err(|e| "can not get base dir. err: ".to_owned() + &e.to_string())?;
+        .map_err(|e| "[ls] can not get base dir. err: ".to_owned() + &e.to_string())?;
 
     let path = std::fs::canonicalize(req.path)
-        .map_err(|e| "path invalid. err: ".to_owned() + &e.to_string())?;
+        .map_err(|e| "[ls] path invalid. err: ".to_owned() + &e.to_string())?;
 
     if !path.starts_with(base.clone()) {
-        return Err("path invalid. err: path not in base dir".to_owned());
+        return Err("[ls] path invalid. err: path not in base dir".to_owned());
     }
 
     let mut files = vec![];
     for entry in std::fs::read_dir(path)
-        .map_err(|e| "can not read dir. err: ".to_owned() + &e.to_string())?
+        .map_err(|e| "[ls] can not read dir. err: ".to_owned() + &e.to_string())?
     {
-        let entry = entry.map_err(|e| "can not read dir. err: ".to_owned() + &e.to_string())?;
+        let entry =
+            entry.map_err(|e| "[ls] can not read dir. err: ".to_owned() + &e.to_string())?;
         let path = entry.path();
         let path = path.strip_prefix(&base).map_err(|e| e.to_string())?;
         if let Some(path) = path.to_str() {
@@ -51,25 +52,25 @@ mod test {
             };
 
             let req = LsRequest { path: "./".into() };
-            let resp = ls(Json(req)).await.expect("expect ok, return err").0;
+            let resp = ls(Query(req)).await.expect("expect ok, return err").0;
 
             assert_eq!(expect, resp);
         }
 
         {
-            let expect = "path invalid. err: path not in base dir";
+            let expect = "[ls] path invalid. err: path not in base dir";
 
             let req = LsRequest { path: "../".into() };
-            let resp = ls(Json(req)).await.expect_err("expect err, return ok");
+            let resp = ls(Query(req)).await.expect_err("expect err, return ok");
 
             assert_eq!(expect, resp);
         }
 
         {
-            let expect = "path invalid. err: No such file or directory (os error 2)";
+            let expect = "[ls] path invalid. err: No such file or directory (os error 2)";
 
             let req = LsRequest { path: "..?".into() };
-            let resp = ls(Json(req)).await.expect_err("expect err, return ok");
+            let resp = ls(Query(req)).await.expect_err("expect err, return ok");
 
             assert_eq!(expect, resp);
         }
